0639e275e56de77b43397fd5de0488d9151aae8e
vicky09teavah <vs1892857@gmail.com>
2025-03-31 01:42:39 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -194,7 +194,7 @@ async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
 async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
-    reply = await m.reply_text(f"**★彡 ᵘᵖˡᵒᵃᵈⁱⁿᵍ 彡★ ...⏳**\n\n**📚𝐓𝐢𝐭𝐥𝐞** » `{name}\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+    reply = await m.reply_text(f"🚀 *Upload in Progress...* ⏳\n\n📡 *Title:* `{name}`\n\n🗝️ *Developed By:* 𝕽𝕠𝕝𝕖𝕩")
     try:
         if thumb == "no":
             thumbnail = f"{filename}.jpg"
